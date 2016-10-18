0d0ef2103ff83736b238329efc860b1553e0bbf7
Richa <singh.richa123495@gmail.com>
2016-10-18 10:08:47 +0530

    Create binary_search.py

---
+++ Algorithms/Search/binary_search.py
@@ -0,0 +1,14 @@
+def binary_search(array, target):
+    lower = 0
+    upper = len(array)
+    while lower < upper:   # use < instead of <=
+        x = lower + (upper - lower) // 2
+        val = array[x]
+        if target == val:
+            return x
+        elif target > val:
+            if lower == x:   # this two are the actual lines
+                break        # you're looking for
+            lower = x
+        elif target < val:
+            upper = x
