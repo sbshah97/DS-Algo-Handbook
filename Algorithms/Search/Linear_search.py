0588411583cba44eef0e6fbd681c21209dfc05f5
Richa <singh.richa123495@gmail.com>
2016-10-18 10:10:44 +0530

    Create Linear_search.py

---
+++ Algorithms/Search/Linear_search.py
@@ -0,0 +1,15 @@
+def search_linear(x,y):
+  n = len( x )
+  for i in range(n):
+
+  if theValue[i] == y:
+        return True
+
+  return false
+
+def main():
+  mainValues =int( input("enter the  array values "))
+  mV = mainValues.list()
+
+  trgt =int( input('enter num to find')
+   search_linear(mainValues, trgt)
