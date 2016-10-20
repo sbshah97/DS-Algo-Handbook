89cfafa69f493f827154d10260222260314474e1
Adrià Ferré Grau <adriaferre@users.noreply.github.com>
2016-10-20 12:04:19 +0200

    DFS algorithm in python

---
+++ Algorithms/DFS.py
@@ -0,0 +1,18 @@
+def dfs(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E']),
+         'G': set()}
+dfs(graph, 'A')
