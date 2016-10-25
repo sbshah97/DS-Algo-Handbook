6912f5f19818a992eff32af96e0d3cbb687e3040
salman-bhai <sbs.191197@gmail.com>
2016-10-25 16:36:06 +0530

    Till Lab 8 completed.

--- lab_assignments/lab8/heap-sort.c
+++ lab_assignments/lab8/heap-sort.c
@@ -1,44 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void heapify(int arr[], int n, int i)
-{
+void heapify(int arr[], int n, int i) {
     int largest = i;  // Initialize largest as root
     int l = 2*i + 1;  // left = 2*i + 1
     int r = 2*i + 2;  // right = 2*i + 2
- 
+
     // If left child is larger than root
     if (l < n && arr[l] > arr[largest])
         largest = l;
- 
+
     // If right child is larger than largest so far
     if (r < n && arr[r] > arr[largest])
         largest = r;
- 
+
     // If largest is not root
-    if (largest != i)
-    {
+    if (largest != i) {
         swap(arr[i], arr[largest]);
- 
+
         // Recursively heapify the affected sub-tree
         heapify(arr, n, largest);
     }
 }
- 
+
 // main function to do heap sort
-void heapSort(int arr[], int n)
-{
+void heapSort(int arr[], int n) {
 	int i;
     // Build heap (rearrange array)
     for ( i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
- 
+
     // One by one extract an element from heap
-    for (i=n-1; i>=0; i--)
-    {
+    for (i=n-1; i>=0; i--) {
         // Move current root to end
         swap(arr[0], arr[i]);
- 
+
         // call max heapify on the reduced heap
         heapify(arr, i, 0);
     }
