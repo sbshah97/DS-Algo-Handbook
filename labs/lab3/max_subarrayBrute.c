b8d65156ba07910e2dd53399a9f3bfcdb7891c69
Salman Shah <sbs.191197@gmail.com>
2017-01-23 15:24:18 +0530

    Lab 3 finished

--- labs/lab3/max_subarrayBrute.c
+++ labs/lab3/max_subarrayBrute.c
@@ -5,25 +5,34 @@ int main() {
 	int n;
 	scanf("%d",&n);
 
-	int arr[n];
+	int arr[n], max2;
 
 	for(int i = 0; i < n ; i ++)
 		scanf("%d",&arr[i]);
-	int max = -99999999;
-	int sum = 0;
+	int max1 = -99999999;
+	// int sum = 0;
+
+	// for(int i = 0; i < n; i ++) {
+	// 	for(int j = i; j < n; j ++) {
+	// 		for(int k = i; k <= j; k++) {
+	// 		 	sum += arr[k];
+	// 		}
+	// 		if(sum > max)
+	// 			max = sum;
+	// 		sum = 0;
+	// 	}
+	// }
 
 	for(int i = 0; i < n; i ++) {
+		max2 = 0;
 		for(int j = i; j < n; j ++) {
-			for(int k = i; k <= j; k++) {
-			 	sum += arr[k];
-			}
-			if(sum > max)
-				max = sum;
-			sum = 0;
+			max2 += arr[j];
+			if(max2 > max1)
+				max1 = max2;
 		}
 	}
 
-	printf("%d\n",max );	
+	printf("%d\n",max1);	
 	
 	return 0;
 }
